f5e2347c2a1f77c7e90f476e1dbfd09f3857c5a8
mayherrr <mehr.khaira.sod21@anu.edu.in>
2023-01-20 23:23:11 +0530

    closest number

---
+++ FS2.js
@@ -0,0 +1,23 @@
+    //const number = parseInt(prompt('Enter the number of terms:'));
+    // parseInt helps extract an element from the string
+
+
+    var a=0;
+    var b=1;
+    var nextTerm
+    
+    var number = prompt('Enter the number of terms:')
+    document.write('Fibonacci Sequence: ');
+    document.write(a, b);
+    nextTerm = a+b;
+
+
+    //for(var i=1; i<=number; i++) {document.write(a); nextTerm= a+b; a=b; b=nextTerm;}
+
+   
+    while(nextTerm <= number) {
+        document.write(nextTerm);
+        a=b;
+        b=nextTerm
+        nextTerm=a+b
+    }
